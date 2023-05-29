843512e187b6103581a3947c0e1f1edef0900781
Zain541 <zainfarooq541@gmail.com>
2023-05-29 22:28:33 +0500

    episode 2 completed

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -13,13 +13,19 @@ class ProjectsTest extends TestCase
     /** @test */
     public function a_user_can_create_a_project()
     {
-        $attributes = [
-            'title' => 'dffffwefef',
-            'description' => 'efrekuwhfgj ekjwfekjwbr',
-        ];
 
-        $this->post('/projects', $attributes);
+        $this->withoutExceptionHandling();
+
+        $attributes = [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+        ];
+    
+
+        $this->post('/projects', $attributes)->assertRedirect('/projects');
 
         $this->assertDatabaseHas('projects', $attributes);
+
+        $this->get('/projects')->assertSee($attributes['title']);
     }
 }
