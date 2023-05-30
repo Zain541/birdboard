2d189e4e50bdb819dd135ea1308dc317dff170d2
Zain541 <zainfarooq541@gmail.com>
2023-05-30 12:55:46 +0500

    testing ssh

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -25,6 +25,7 @@ class ProjectsTest extends TestCase
         $this->post('/projects', $attributes)->assertRedirect('/projects');
 
         $this->assertDatabaseHas('projects', $attributes);
+        
 
         $this->get('/projects')->assertSee($attributes['title']);
     }
